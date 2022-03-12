ca5dc6ed6d5a18a1067b587a0033a62a689e0218
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2022-03-12 00:19:59 -0700

    mongodb Setup

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connect.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
@@ -14,6 +18,15 @@ app.use(errorHandleMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
