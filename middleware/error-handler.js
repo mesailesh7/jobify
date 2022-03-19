bde7865a838b8fe252c1efac364cd7b1fe34c5df
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2022-03-19 08:13:16 -0600

    error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,9 +4,23 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const defaultError = {
     StatusCodes: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong , try again later",
+    msg: err.message || "Something went wrong , try again later",
   };
-  res.status(defaultError.StatusCodes).json({ msg: err });
+  if (err.name == "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} Field has to be unique`;
+  }
+
+  // res.status(defaultError.StatusCodes).json({ msg: err });
+  res.status(defaultError.StatusCodes).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
